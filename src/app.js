1992c884143c00135af6b66d9b5c372de91ee7e5
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-04-26 17:35:24 -0300

    Requisito 1

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { productRouter } = require('./routers');
 
 const app = express();
 
@@ -7,7 +8,11 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionarr
+app.use(express.json());
+
+app.use('/products', productRouter);
+
+// não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
---
+++ src/controllers/product.controller.js
@@ -0,0 +1,20 @@
+const { productService } = require('../services');
+const errorMap = require('../utils/errorMap');
+
+const findAll = async (_req, res) => {
+  const { message } = await productService.findAll();
+  return res.status(200).json(message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productService.findById(id);
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message });
+  return res.status(200).json(message);
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
