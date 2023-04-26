800b3ff9a4b257a72d88c4e1de9c1896d029506d
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-04-26 20:32:26 -0300

    Requisito 4

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { productController } = require('../controllers');
+const validateProductFields = require('../middlewares/validateProductFields');
 
 const router = express.Router();
+
+router.use(validateProductFields);
 
 router.get('/', productController.findAll);
 
